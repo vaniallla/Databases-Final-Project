76718f75c89c26f1707dc0477a853e8e67f057ab
Vanessa Gao <vgao825@gmail.com>
2024-05-02 14:47:14 -0400

    cleaned up app

--- src/App.js
+++ src/App.js
@@ -3,7 +3,16 @@ import React from "react";
 function App() {
   return (
     <div>
-      <h1>Hello, World!</h1>
+      <header>
+        <h1>Burger Restaurant</h1>
+      </header>
+      <main>
+        <div className="buttons-container">
+          <button>Employees</button>
+          <button>Inventory</button>
+          <button>Vendors</button>
+        </div>
+      </main>
     </div>
   );
 }
